7fc7da267b158a07875e0f13bd625a2e445940ac
MounikaDeveloper <57697586+MounikaDeveloper@users.noreply.github.com>
2022-01-10 16:24:50 +0530

    Update client1.py

--- client1.py
+++ client1.py
@@ -1,7 +1,18 @@
 import socket
 PORT=5050
+def extract_ip():
+    st=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    try:
+        st.connect(('10.255.255.255',1))
+        ip=st.getsockname()[0]
+    except Exception:
+        ip='127.0.0.1'
+    finally:
+        st.close()
+    return ip
+HOST=extract_ip()
 #HOST =socket.gethostbyname(socket.gethostname())
-HOST='172.18.144.2'
+#HOST='172.18.144.2'
 client = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
 client.connect((HOST,PORT))
 #print(c.rev(1024).decode())
