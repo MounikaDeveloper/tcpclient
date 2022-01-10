30c438bfc98d97354d36d8f0be10a36bb9818d3c
MounikaDeveloper <57697586+MounikaDeveloper@users.noreply.github.com>
2022-01-10 15:14:01 +0530

    Update client1.py

--- client1.py
+++ client1.py
@@ -1,6 +1,7 @@
 import socket
 PORT=5050
 HOST =socket.gethostbyname(socket.gethostname())
-c.connect((HOST,PORT))
+client = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+client.connect((HOST,PORT))
 #print(c.rev(1024).decode())
 
